3bb9b5a67bcfd4cc907d81b3e2f3fa90e624f76a
Ameth Ba <amethba8826@gmail.com>
2025-11-27 17:14:38 +0000

    `update`

--- src/app/features/thankyou/thankyou.ts
+++ src/app/features/thankyou/thankyou.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import lottie from 'lottie-web';
+
+import * as successAnimationData from '../../../assets/lottie/404.json';
 
 @Component({
   selector: 'app-thankyou',
+  standalone: true,
   imports: [],
   templateUrl: './thankyou.html',
-  styleUrl: './thankyou.css'
+  styleUrl: './thankyou.css',
 })
-export class ThankYou {
+export class ThankYou implements AfterViewInit {
+  private readonly lottieAnimationData: any = successAnimationData;
 
+  ngAfterViewInit(): void {
+    this.loadLottieAnimation();
+  }
+
+  loadLottieAnimation(): void {
+    const container = document.getElementById('lottie-success-animation');
+
+    const lottieInstance = (lottie as any).default || lottie;
+    const animationData =
+      this.lottieAnimationData.default || this.lottieAnimationData;
+
+    if (container && animationData) {
+      lottieInstance.loadAnimation({
+        container: container,
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        animationData: animationData,
+      });
+    }
+  }
 }
